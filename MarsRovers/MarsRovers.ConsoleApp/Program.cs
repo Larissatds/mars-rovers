04537f69be22e55f075b9887d9aa889856767dac
Larissa Teixeira <tdslari@gmail.com>
2025-06-15 20:05:55 -0300

    implementação da program e services para intermediar e validar as requisições

--- MarsRovers/MarsRovers.ConsoleApp/Program.cs
+++ MarsRovers/MarsRovers.ConsoleApp/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using MarsRovers.Models;
+using MarsRovers.Services;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Informe o tamanho do planalto (exemplo: 5 5):");
+        var plateauInput = Console.ReadLine();
+        var plateauCoords = plateauInput.Split(' ');
+
+        var plateau = new Plateau(int.Parse(plateauCoords[0]), int.Parse(plateauCoords[1]));
+
+        var rovers = new List<Rover>();
+        var roverService = new RoverService(plateau);
+
+        while (true)
+        {
+            Console.WriteLine("Informe posição inicial da sonda (exemplo: 1 2 N), ou vazio para sair:");
+            var posInput = Console.ReadLine();
+            if (string.IsNullOrWhiteSpace(posInput))
+                break;
+
+            var posParts = posInput.Split(' ');
+            var x = int.Parse(posParts[0]);
+            var y = int.Parse(posParts[1]);
+            var dir = Enum.Parse<Direction>(posParts[2].ToUpper(), true);
+
+            try
+            {
+                var rover = roverService.CreateRover(new Position(x, y), dir);
+
+                Console.WriteLine("Informe comandos da sonda (exemplo: LMLMLMLMM):");
+                var commands = Console.ReadLine();
+
+                roverService.ExecuteCommands(rover, commands);
+
+                rovers.Add(rover);
+                Console.WriteLine($"Posição final da sonda: {rover.Position.X} {rover.Position.Y} {rover.Direction.ToString()[0]}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erro ao mover a sonda: {ex.Message}");
+            }
+        }
+
+        Console.WriteLine("Fim das sondas.");
+    }
+}
---
+++ MarsRovers/MarsRovers/Services/RoverService.cs
@@ -0,0 +1,55 @@
+﻿using MarsRovers.Commands;
+using MarsRovers.Models;
+
+namespace MarsRovers.Services;
+
+public class RoverService
+{
+    private readonly Plateau _plateau;
+
+    public RoverService(Plateau plateau)
+    {
+        _plateau = plateau;
+    }
+
+    public Rover CreateRover(Position position, Direction direction)
+    {
+        if (!_plateau.IsWithinBounds(position))
+            throw new Exception("Posição inicial fora dos limites");
+
+        if (_plateau.IsOccupied(position))
+            throw new Exception("Posição já ocupada");
+
+        var rover = new Rover(position, direction);
+        _plateau.Occupy(position);
+        return rover;
+    }
+
+    public void ExecuteCommands(Rover rover, string commandSequence)
+    {
+        foreach (char c in commandSequence)
+        {
+            ICommand command = c switch
+            {
+                'L' => new TurnLeftCommand(),
+                'R' => new TurnRightCommand(),
+                'M' => new MoveCommand(),
+                _ => throw new Exception($"Comando inválido: {c}")
+            };
+
+            command.Execute(rover, _plateau);
+
+            if (!_plateau.IsWithinBounds(rover.Position))
+            {
+                Console.WriteLine("Movimento resultou fora dos limites do planalto, comando ignorado.");
+                break;
+            }
+
+            if (_plateau.IsOccupied(rover.Position))
+            {
+                Console.WriteLine("Movimento resultou em colisão com outra sonda, comando ignorado.");
+                break;
+            }
+        }
+    }
+}
